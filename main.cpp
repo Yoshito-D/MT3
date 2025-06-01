e3d0078291a2ef931cbc87c8f934f1447d92f41a
Yoshito17 <k024g0035@g.neec.ac.jp>
2025-06-02 01:22:31 +0900

    04_00確認課題

--- main.cpp
+++ main.cpp
@@ -25,16 +25,39 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	char keys[256] = { 0 };
 	char preKeys[256] = { 0 };
 
-	Vector3 a{ 0.2f,1.0f,0.0f };
-	Vector3 b{ 2.4f,3.1f,1.2f };
-	Vector3 c = a + b;
-	Vector3 d = a - b;
-	Vector3 e = a * 2.4f;
-	Vector3 rotate{ 0.4f,1.43f,-0.8f };
-	Matrix4x4 rotateXMatrix = MakeRotateXMatrix(rotate.x);
-	Matrix4x4 rotateYMatrix = MakeRotateYMatrix(rotate.y);
-	Matrix4x4 rotateZMatrix = MakeRotateZMatrix(rotate.z);
-	Matrix4x4 rotateMatrix = rotateXMatrix * rotateYMatrix * rotateZMatrix;
+	Vector3 cameraScale = { 1.0f,1.0f,1.0f };
+	Vector3 cameraRotate = { 0.26f,0.0f,0.0f };
+	Vector3 cameraPosition = { 0.0f,2.0f,-6.5f };
+	Matrix4x4 cameraMatrix = MakeAffineMatrix(cameraScale, cameraRotate, cameraPosition);
+	Matrix4x4 viewMatrix = cameraMatrix.Inverse();
+	Matrix4x4 projectionMatrix = MakePerspectiveFovMatrix(0.45f, kWindowWidth / kWindowHeight, 0.1f, 100.0f);
+	Matrix4x4 viewportMatrix = MakeViewportMatrix(0.0f, 0.0f, kWindowWidth, kWindowHeight, 0.0f, 1.0f);
+
+	bool isDebugCamera = true;
+
+	Spring spring{};
+	spring.anchor = { 0.0f, 0.0f, 0.0f };
+	spring.naturalLength = 1.0f;
+	spring.stiffness = 100.0f;
+	spring.dampingCoefficient = 2.0f;
+
+	Ball ball{};
+	ball.position = { 1.2f, 0.0f, 0.0f };
+	ball.mass = 2.0f;
+	ball.radius = 0.05f;
+	ball.color = BLUE;
+
+	float deltaTime = 1.0f / 60.0f;
+
+	Sphere sphere{};
+	sphere.center = ball.position;
+	sphere.radius = ball.radius;
+
+	Segment segment{};
+	segment.origin = spring.anchor;
+	segment.diff = ball.position - spring.anchor;
+
+	bool isStrated = false;
 
 	// ウィンドウの×ボタンが押されるまでループ
 	while (Novice::ProcessMessage() == 0) {
@@ -49,9 +72,72 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		/// ↓更新処理ここから
 		///
 
+		if (keys[DIK_SPACE] && !preKeys[DIK_SPACE]) {
+			isDebugCamera = !isDebugCamera;
+		}
+
+		if (isDebugCamera) {
+			if (Novice::IsPressMouse(2)) {
+				ImVec2 delta = ImGui::GetIO().MouseDelta;
+				if (delta.x != 0.0f || delta.y != 0.0f) {
+					float moveSpeed = 0.005f;
+
+					Vector3 forward = { cosf(cameraRotate.x) * sinf(cameraRotate.y),
+						sinf(cameraRotate.x),
+						cosf(cameraRotate.x) * cosf(cameraRotate.y)
+					};
+
+					Vector3 up = { 0.0f,1.0f,0.0f };
+					Vector3 right = up.Cross(forward).Normalize();
+
+					Vector3 move = -right * delta.x * moveSpeed + up * delta.y * moveSpeed;
+
+					cameraPosition += move;
+				}
+			}
+
+			if (Novice::IsPressMouse(1)) {
+				ImVec2 delta = ImGui::GetIO().MouseDelta;
+				if (delta.x != 0.0f || delta.y != 0.0f) {
+					float rotateSpeed = 0.001f;
+
+					cameraRotate.x += delta.y * rotateSpeed;
+					cameraRotate.y += delta.x * rotateSpeed;
+				}
+			}
+		}
+
+		cameraMatrix = MakeAffineMatrix(cameraScale, cameraRotate, cameraPosition);
+		viewMatrix = cameraMatrix.Inverse();
+		projectionMatrix = MakePerspectiveFovMatrix(0.45f, kWindowWidth / kWindowHeight, 0.1f, 100.0f);
+
+		if (isStrated) {
+			Vector3 diff = ball.position - spring.anchor;
+			float length = diff.Length();
+			if (length != 0.0f) {
+				Vector3 direction = diff.Normalize();
+				Vector3 restPosition = spring.anchor + direction * spring.naturalLength;
+				Vector3 displacement = (ball.position - restPosition) * length;
+				Vector3 restoringForce = displacement * (-spring.stiffness);
+				Vector3 dampingForce = ball.velocity * (-spring.dampingCoefficient);
+				Vector3 force = restoringForce + dampingForce;
+				ball.acceleration = force / ball.mass;
+			}
+
+			ball.velocity += ball.acceleration * deltaTime;
+			ball.position += ball.velocity * deltaTime;
+
+			sphere.center = ball.position;
+			segment.diff = ball.position - spring.anchor;
+		}
+
 		///
 		/// ↑更新処理ここまで
 		///
+
+		Draw::DrawGrid((viewMatrix * projectionMatrix), viewportMatrix);
+		Draw::DrawSphere(sphere, (viewMatrix * projectionMatrix), viewportMatrix, ball.color);
+		Draw::DrawSegment(segment, (viewMatrix * projectionMatrix), viewportMatrix, WHITE);
 
 		///
 		/// ↓描画処理ここから
@@ -59,15 +145,14 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 #ifdef _DEBUG
 		ImGui::Begin("Window");
-		ImGui::Text("c:%f, %f, %f", c.x, c.y, c.z);
-		ImGui::Text("d:%f, %f, %f", d.x, d.y, d.z);
-		ImGui::Text("e:%f, %f, %f", e.x, e.y, e.z);
-		ImGui::Text("matrix:\n%f, %f, %f, %f\n%f, %f, %f, %f\n%f, %f, %f, %f\n%f, %f, %f, %f",
-			rotateMatrix.m[0][0], rotateMatrix.m[0][1], rotateMatrix.m[0][2], rotateMatrix.m[0][3],
-			rotateMatrix.m[1][0], rotateMatrix.m[1][1], rotateMatrix.m[1][2], rotateMatrix.m[1][3],
-			rotateMatrix.m[2][0], rotateMatrix.m[2][1], rotateMatrix.m[2][2], rotateMatrix.m[2][3],
-			rotateMatrix.m[3][0], rotateMatrix.m[3][1], rotateMatrix.m[3][2], rotateMatrix.m[3][3]
-		);
+		if (ImGui::Button("Start")) {
+			isStrated = true;
+			spring.anchor = { 0.0f, 0.0f, 0.0f };
+			spring.naturalLength = 1.0f;
+			spring.stiffness = 100.0f;
+			spring.dampingCoefficient = 2.0f;
+			ball.position = { 1.2f, 0.0f, 0.0f };
+		}
 		ImGui::End();
 #endif // _DEBUG
 
